9ce684b3edce3a015ac9f5db001dd3189e112abf
jay-1307 <jayparekh56@gmail.com>
2017-05-12 22:51:25 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,3 +1,4 @@
 #this file contains the python code of our future ocr project.
 #just to see whether it saw changes or not
-#made some changes in reviewable and seeing if anything comes up there
+#made some changes in reviewable.
+#getting no change in reviewable, again trying what else can be done
